9baacbce2a50fac8aad19a80f7277ce87d00b369
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2023-02-21 21:59:00 +0900

    [Bronze V] Title: 영수증, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const total = Number(input[0])
+let sum = 0
+
+for(let i = 2; i <= Number(input[1]) + 1; i++) {
+    let arr = input[i].split(" ").map(Number)
+    sum += arr[0] * arr[1]
+}
+
+if(total === sum) {
+  console.log('Yes')
+} else {
+  console.log('No')
+}
