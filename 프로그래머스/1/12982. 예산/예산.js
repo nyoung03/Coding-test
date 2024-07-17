4acc3a3ba7b144ed6b6b5f2eef65e2f49bc50a40
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2024-07-18 00:13:27 +0900

    [level 1] Title: 예산, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,18 +1,14 @@
 function solution(d, budget) {
-    var answer = budget;
     let sortArr = d.sort((a,b) => a - b)
     let count = 0
     
-    for(let i = 0; i < d.length; i++) {
-        answer -= sortArr[i]
-        
-        if(answer >= 0) {
+    for (let i = 0; i < d.length; i++) {
+        budget -= sortArr[i]        
+        if (budget >= 0) {
             count += 1
+            continue;
         }
-        
-        if(answer <= 0) {
-            break;
-        }
+        break;
     }
     return count;
 }
