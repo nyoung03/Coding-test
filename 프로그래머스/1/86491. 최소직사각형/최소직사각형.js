d00145cf6adeacbb7ed4b3f1aa7a849d6dbfab9e
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2024-07-24 00:26:36 +0900

    [level 1] Title: 최소직사각형, Time: 3.22 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,16 +1,26 @@
 function solution(sizes) {
-    let x = []
-    let y = []
+    let width = 0;
+    let height = 0;
     
-    for(let i = 0; i < sizes.length; i++) {
-        if(sizes[i][0] >= sizes[i][1]) {
-            x.push(sizes[i][0])
-            y.push(sizes[i][1])
-        } else {
-            x.push(sizes[i][1])
-            y.push(sizes[i][0])
-        }
+    for (let i = 0; i < sizes.length; i++) {
+        width = Math.max(width, Math.max(sizes[i][0], sizes[i][1]));
+        height = Math.max(height, Math.min(sizes[i][0], sizes[i][1]));
     }
+    return width * height;
     
-    return Math.max(...x) * Math.max(...y);
+    
+//     let x = []
+//     let y = []
+    
+//     for(let i = 0; i < sizes.length; i++) {
+//         if(sizes[i][0] >= sizes[i][1]) {
+//             x.push(sizes[i][0])
+//             y.push(sizes[i][1])
+//         } else {
+//             x.push(sizes[i][1])
+//             y.push(sizes[i][0])
+//         }
+//     }
+    
+//     return Math.max(...x) * Math.max(...y);
 }
