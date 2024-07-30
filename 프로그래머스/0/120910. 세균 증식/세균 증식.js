a939eef29017861fbe024b4184470881eb138b45
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2024-07-30 22:24:32 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.js
@@ -1,9 +1,4 @@
 function solution(n, t) {
-    var answer = 0;
-    
-    for(let i = 1; i <= t; i++) {
-        answer = (n * (2 ** i))
-    }
-    
-    return answer;
+
+    return n * (2 ** t);
 }
