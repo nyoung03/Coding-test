ef18d58b36dec03565517fa8e63b385ccb5df2cd
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2024-07-30 23:11:58 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,15 @@
+function solution(price) {
+    let amount = 0
+    
+    if(price >= 500000) {
+        amount = price - (price * 0.2)
+    } else if(price >= 300000) {
+        amount = price - (price * 0.1)
+    } else if(price >= 100000) {
+        amount = price - (price * 0.05)
+    } else {
+        amount = price
+    }
+    
+    return Math.floor(amount)
+}
