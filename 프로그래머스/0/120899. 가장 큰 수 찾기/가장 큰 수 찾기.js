fee26691049cbb91ff0d93da0a7a872c8ba5007a
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2024-08-07 23:05:56 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,19 +1,5 @@
 function solution(array) {
-    let temp = 0
-    let index = 0
-    
-    for(let i = 0; i < array.length; i++) {
-        if(temp <= array[i]) {
-            temp = array[i]
-        }
-    }
-    
-    for(let i = 0; i < array.length; i++) {
-        if(temp === array[i]) {
-            index = i
-        }
-    }
-    
+    let max = Math.max(...array)
    
-    return [temp, index];
+    return [max, array.indexOf(max)];
 }
