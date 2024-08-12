e02ce3e27b0bb590ee0d94f28dc74ea1aac7d2bf
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2024-08-12 20:43:27 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,6 +1,5 @@
 function solution(s1, s2) {
-    let answer = 0
-    s1.map(i => s2.includes(i) ? answer += 1 : answer += 0);
-    
-    return answer;
+    let answer = s1.filter(i => s2.includes(i))
+   
+    return answer.length;
 }
