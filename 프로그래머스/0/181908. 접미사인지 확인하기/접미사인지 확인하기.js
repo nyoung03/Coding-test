11051a936dd21e530eab891f2324f9ec2725cb45
nyoung03 <87607036+nyoung03@users.noreply.github.com>
2024-08-15 22:29:55 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,4 +1,4 @@
 function solution(my_string, is_suffix) {
     
-    return my_string.split('').reverse().join('').slice(0, is_suffix.length).split('').reverse().join('') === is_suffix ? 1 : 0;
+    return my_string.endsWith(is_suffix) ? 1 : 0
 }
